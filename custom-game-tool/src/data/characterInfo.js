029733efd331c07dd9f9c2b6247409448f590bdf
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2025-10-21 12:17:57 +0900

    [FEAT] add character info

---
+++ custom-game-tool/src/data/characterInfo.js
@@ -0,0 +1,228 @@
+export const characterInfo = {
+  tank: [
+    {
+      name: "디바",
+      color: "",
+      img: "",
+    },
+    {
+      name: "둠피스트",
+      color: "",
+      img: "",
+    },
+    {
+      name: "라마트라",
+      color: "",
+      img: "",
+    },
+    {
+      name: "라인하르트",
+      color: "",
+      img: "",
+    },
+    {
+      name: "레킹볼",
+      color: "",
+      img: "",
+    },
+    {
+      name: "로드호그",
+      color: "",
+      img: "",
+    },
+    {
+      name: "마우가",
+      color: "",
+      img: "",
+    },
+    {
+      name: "시그마",
+      color: "",
+      img: "",
+    },
+    {
+      name: "오리사",
+      color: "",
+      img: "",
+    },
+    {
+      name: "윈스턴",
+      color: "",
+      img: "",
+    },
+    {
+      name: "지라야",
+      color: "",
+      img: "",
+    },
+    {
+      name: "정커퀸",
+      color: "",
+      img: "",
+    },
+    {
+      name: "해저드",
+      color: "",
+      img: "",
+    },
+  ],
+  dps: [
+    {
+      name: "겐지",
+      color: "",
+      img: "",
+    },
+    {
+      name: "리퍼",
+      color: "",
+      img: "",
+    },
+    {
+      name: "메이",
+      color: "",
+      img: "",
+    },
+    {
+      name: "바스티온",
+      color: "",
+      img: "",
+    },
+    {
+      name: "벤처",
+      color: "",
+      img: "",
+    },
+    {
+      name: "소전",
+      color: "",
+      img: "",
+    },
+    {
+      name: "솔저:76",
+      color: "",
+      img: "",
+    },
+    {
+      name: "솜브라",
+      color: "",
+      img: "",
+    },
+    {
+      name: "시메트라",
+      color: "",
+      img: "",
+    },
+    {
+      name: "애쉬",
+      color: "",
+      img: "",
+    },
+    {
+      name: "에코",
+      color: "",
+      img: "",
+    },
+    {
+      name: "위도우메이커",
+      color: "",
+      img: "",
+    },
+    {
+      name: "정크랫",
+      color: "",
+      img: "",
+    },
+    {
+      name: "캐서디",
+      color: "",
+      img: "",
+    },
+    {
+      name: "토르비욘",
+      color: "",
+      img: "",
+    },
+    {
+      name: "트레이서",
+      color: "",
+      img: "",
+    },
+    {
+      name: "파라",
+      color: "",
+      img: "",
+    },
+    {
+      name: "프레야",
+      color: "",
+      img: "",
+    },
+    {
+      name: "한조",
+      color: "",
+      img: "",
+    },
+  ],
+  healer: [
+    {
+      name: "우양",
+      color: "",
+      img: "",
+    },
+    {
+      name: "라이프위버",
+      color: "",
+      img: "",
+    },
+    {
+      name: "루시우",
+      color: "",
+      img: "",
+    },
+    {
+      name: "메르시",
+      color: "",
+      img: "",
+    },
+    {
+      name: "모이라",
+      color: "",
+      img: "",
+    },
+    {
+      name: "바티스트",
+      color: "",
+      img: "",
+    },
+    {
+      name: "브리기테",
+      color: "",
+      img: "",
+    },
+    {
+      name: "아나",
+      color: "",
+      img: "",
+    },
+    {
+      name: "일리아리",
+      color: "",
+      img: "",
+    },
+    {
+      name: "젠야타",
+      color: "",
+      img: "",
+    },
+    {
+      name: "주노",
+      color: "",
+      img: "",
+    },
+    {
+      name: "키리코",
+      color: "",
+      img: "",
+    },
+  ],
+};
